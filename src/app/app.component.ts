beaf55fa386298a4e568cefca958db13719677cf
QuangNguyen16 <nguyenvangiang2k.00@gmail.com>
2020-08-07 14:34:29 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: '[app-root]',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fontend';
+  title = 'app-demo';
+  constructor(public router: Router,) {
+    this.router.events.subscribe((evt) => {
+      if ((evt instanceof NavigationStart)) {
+        // javascript
+        setTimeout(() => {
+          let el = document.getElementById('loading');
+          if (el) {
+            el.remove()
+          }
+        }, 9000);
+      }
+    });
+  }
 }
